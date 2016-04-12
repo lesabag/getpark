224b21388832dce43fe1ec59d5e03f86e12738ed
lesabag <lioresabag@gmail.com>
2016-04-13 00:38:52 +0300

    Added files via upload
    Add proj

---
+++ src/main/java/home/getpark/GcmBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package home.getpark;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by home on 11/04/2016.
+ */
+public class GcmBroadcastReceiver  extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),  GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
